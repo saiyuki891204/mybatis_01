d15258f1c93bae3372aefefe48b56a1875ec6c72
guochen <bvg_winnir@163.com>
2019-06-22 22:34:08 +0800

    Initial commit

---
+++ src/main/java/com/cx/mybatis/test/MyBatisTest.java
@@ -0,0 +1,27 @@
+package com.cx.mybatis.test;
+
+import com.cx.mybatis.bean.Employee;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MyBatisTest {
+
+    @Test
+    public void test() throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        Employee employee = sqlSession.selectOne("selectEmp",1);
+
+        sqlSession.close();
+        System.out.println(employee.toString());
+    }
+}
