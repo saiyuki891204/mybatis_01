14514b94cae65682aa6c66e64bb537f0428f5651
guochen <bvg_winnir@163.com>
2019-06-23 20:07:34 +0800

    demo1

--- src/main/java/com/cx/mybatis/test/MyBatisTest.java
+++ src/main/java/com/cx/mybatis/test/MyBatisTest.java
@@ -1,6 +1,8 @@
 package com.cx.mybatis.test;
 
 import com.cx.mybatis.bean.Employee;
+import com.cx.mybatis.dao.mapper.EmployeeMapper;
+import javafx.scene.chart.PieChart;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -9,6 +11,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
 
 public class MyBatisTest {
 
@@ -23,5 +26,24 @@ public class MyBatisTest {
 
         sqlSession.close();
         System.out.println(employee.toString());
+    }
+
+    @Test
+    public void test01() throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        try {
+            EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
+            Employee employee = mapper.getEmployeeById(1);
+
+            System.out.println(employee);
+        }finally {
+            sqlSession.close();
+        }
+
     }
 }
